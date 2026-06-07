de9db36ec2ee98f61328299bffa285042caf9ff8
niharikathunga-sudo <niharikathunga@gmail.com>
2026-06-07 12:15:47 -0500

    Add files via upload

--- recylegame2.py
+++ recylegame2.py
@@ -1,6 +1,6 @@
 import pygame,random,time
 pygame.init()
-screen=pygame.display.set_mode((864,850))
+screen=pygame.display.set_mode((864,750))
 clock=pygame.time.Clock()
 pygame.display.set_caption("Welcome!!!")
 score=0
@@ -43,15 +43,15 @@ all_group=pygame.sprite.Group()
 
 for i in range(50):
     item=recycleable(random.choice(recycleable1))
-    item.rect.x=random.randrange(864)
-    item.rect.y=random.randrange(850)
+    item.rect.x=random.randrange(844)
+    item.rect.y=random.randrange(730)
     r_group.add(item)
     all_group.add(item)
 
 for i in range(20):
     plastic=nonrecycleable()
-    plastic.rect.x=random.randrange(864)
-    plastic.rect.y=random.randrange(850)
+    plastic.rect.x=random.randrange(844)
+    plastic.rect.y=random.randrange(730)
     nr_group.add(plastic)
     all_group.add(plastic)
 
@@ -75,8 +75,38 @@ while True:
 
     else:
         screen.fill("light pink")
-        text=font.render("time"+str(past),True,"black")
-        screen.blit(text,(70,100))
+        text=font.render("Time "+str(round(past,0)),True,"black")
+        screen.blit(text,(50,50))
+
+        keys=pygame.key.get_pressed()
+
+        if keys[pygame.K_UP] and trash.rect.y>0:
+            trash.rect.y-=5
+        if keys[pygame.K_DOWN] and trash.rect.y<720:
+            trash.rect.y+=5
+        if keys[pygame.K_LEFT] and trash.rect.x>0:
+            trash.rect.x-=5
+        if keys[pygame.K_RIGHT] and trash.rect.x<844:
+            trash.rect.x+=5
+
+        #check for collision
+        rhitlist=pygame.sprite.spritecollide(trash,r_group,True)
+        nrhitlist=pygame.sprite.spritecollide(trash,nr_group,True)
+
+        for item in rhitlist:
+            score+=1
+
+        for item in nrhitlist:
+            score-=2
+        
+        text=font.render("Score "+str(score),True,"black")
+        screen.blit(text,(50,90))
+
+
+            
+        
+
+
 
     all_group.draw(screen)
     pygame.display.update()
